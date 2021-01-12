d9da72098e205d79bc15b8ccaba9848ecb94cb3d
Ernest Nezic <ernest.nezic@gmail.com>
2021-01-12 14:20:14 +0100

    Exceeds Expectations (Unreviewed Version)

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # techdegree-project-6
  Treehouse Full Stack JavaScript Techdegree- Project 6: Static Node.js and Express Site
+
+Student:
+    *Ernest Nežić
+
+Project:
+    * Treehouse Techdegree Unit 6:
+        Static Node.js and Express Site
+
+Going for:
+    *Exceeds Expectations
+
+Changed styles:
+    *color (text in left pane)
+    *background_color (left pane)
+    *box shadow (left pane)
+
+project description:
+    *A portfolio website to showcase all project done for the Techdegree until now
+
+Cross-browser consistency:
+    *Google Chrome for Mac Version 84.0.4147.105
+    *Safari Version 13.1.2
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,29 @@
+//Requiring Express framework and using it in the app
 const express = require('express');
 const router = express.Router();
 
+//Requiring the data.json file containing all of the data for our project, getting its projects object in which an array of projects is stored
 const { projects }  = require('../data.json');
 
 
+//Setting the root route for the application
 router.get('/', (req, res) => {
     res.render('index', { projects });
 });
 
+
+//Setting the about route for the applicaiton
 router.get('/about', (req, res) => {
     res.render('about');
 });
 
+//Handling requests for specific projects routes
 router.get('/project/:id', (req, res) => {
     const projectId = req.params.id;
     if ( projectId >= 0 && projectId <= projects.length) {
         res.render('project', {project : projects[projectId]});
     } else {
+        //Handling wrong project requests with 404 - Not Found error
         const err = new Error();
         err.status = 404;
         err.message = '404 - Page Not Found!'
@@ -26,5 +33,5 @@ router.get('/project/:id', (req, res) => {
     
 });
 
-
+//Exporting the index.js file to be used in the main, app.js file
 module.exports = router;
