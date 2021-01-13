4438f0468701b313c6da16a3a2a5f494d93dd4b5
Ernest Nezic <ernest.nezic@gmail.com>
2021-01-13 03:58:41 +0100

    Final Project Version

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
 //Requiering Express Framework and using it in the app
-const express = require('express');
+const express = require( 'express' );
 const app = express();
 
+
 //Setting the markup language( view engine ) to Pug
-app.set('view engine', 'pug');
+app.set( 'view engine', 'pug' );
+
 
 //Declaring the static assets (CSS, images...)
-app.use('/static', express.static('public'));
+app.use( '/static', express.static( 'public' ) );
+
 
 //Including routes file in main app.js file
-const routes = require('./routes');
-app.use(routes);
+const routes = require( './routes' );
+app.use( routes );
 
 //Middleware function to handle the 404 Errors outside the 'project' route
-app.use((req, res, next) => {
-    const err = new Error('Not found!')
+app.use( ( req, res, next ) => {
+    const err = new Error( 'Not found!' );
     err.status = 404;
-    next(err)
-});
+    next( err );
+} );
 
 //Middleware function to handle all errors coused during the apps running
-app.use((err, req, res, next) => {
+app.use( ( err, req, res, next ) => {
     
     //Handling 404 - Not Found errors
-    if (err.status === 404) {
-        err.message = '404 - Page Not Found!'
-        res.render('page-404', { status : err.message })
+    if ( err.status === 404 ) {
+        err.message = 'Page Not Found!';
+        console.log( `${ err.status } - ${ err.message }` );
+        res.render( 'page-not-found', { status: err } );
     } else {
         //Handling server errors
-        err.message = ':( Looks like something went wrong on the Server, please try reloading the page!'
+        err.message = ':( Looks like something went wrong on the Server, please try reloading the page!';
         err.status = 500;
-        res.status(err.status);
-        res.render('error', err)
+        res.status( err.status );
+        res.render( 'error', err );
     }
      
-});
+} );
 
 //Adding listening port through which we can launch the app
-app.listen(3000, () => {
-    console.log('The App is running on localhost:3000');
-})
+app.listen( 3000, () => {
+    console.log( 'The App is running on localhost:3000' );
+} );
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,36 @@
 //Requiring Express framework and using it in the app
-const express = require('express');
+const express = require( 'express' );
 const router = express.Router();
 
+
 //Requiring the data.json file containing all of the data for our project, getting its projects object in which an array of projects is stored
-const { projects }  = require('../data.json');
+const { projects }  = require( '../data.json' );
 
 
 //Setting the root route for the application
-router.get('/', (req, res) => {
-    res.render('index', { projects });
-});
-
+router.get( '/', ( req, res ) => {
+    res.render( 'index', { projects } );
+} );
 
 //Setting the about route for the applicaiton
-router.get('/about', (req, res) => {
-    res.render('about');
-});
+router.get( '/about', ( req, res ) => {
+    res.render( 'about' );
+} );
 
 //Handling requests for specific projects routes
-router.get('/project/:id', (req, res) => {
+router.get( '/project/:id', ( req, res ) => {
     const projectId = req.params.id;
-    if ( projectId >= 0 && projectId <= projects.length) {
-        res.render('project', {project : projects[projectId]});
+    if ( projectId >= 0 && projectId <= projects.length ) {
+        res.render( 'project', { project : projects[projectId] });
     } else {
         //Handling wrong project requests with 404 - Not Found error
         const err = new Error();
         err.status = 404;
-        err.message = '404 - Page Not Found!'
-        res.render('page-404', { status : err.message })
-    }
-    
-    
-});
+        err.message = 'Page Not Found!';
+        res.render( 'page-not-found', { status : err });
+    } 
+} );
+
 
 //Exporting the index.js file to be used in the main, app.js file
 module.exports = router;
